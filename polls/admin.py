1bf5b27637683ff32fe098900f4cb86487b76fab
Nguyen Luong Bang <luongbangvh@gmail.com>
2016-05-29 23:20:04 +0700

    add admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
+                 ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+    search_fields = ['question_text']
+    list_per_page = 5
+
+
+admin.site.register(Question, QuestionAdmin)
