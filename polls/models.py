01585df183b6a51b77d4c602ab41bae74a5b4e93
Nguyen Luong Bang <luongbangvh@gmail.com>
2016-06-09 23:12:51 +0700

    add model

--- polls/models.py
+++ polls/models.py
@@ -21,3 +21,47 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large')
+    )
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField
+    num_stars = models.IntegerField
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    topping = models.ManyToManyField(Topping)
+
+
